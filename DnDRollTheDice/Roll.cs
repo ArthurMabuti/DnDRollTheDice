38e8446e7f549b3f57bbb61c6ae51dba7559383c
ArthurMabuti <arthurmabuti@hotmail.com>
2023-12-26 11:21:11 -0300

    Created DiceRoll() function

--- DnDRollTheDice/Roll.cs
+++ DnDRollTheDice/Roll.cs
@@ -4,4 +4,11 @@ internal class Roll
 {
     public int Sides { get; set; }
     public int NumberOfDices { get; set; }
+
+    public int DiceRoll(int numberOfDices, int sides)
+    {
+        Random random = new Random();
+        int diceRoll = random.Next(1, sides + 1);
+        return diceRoll;
+    }
 }
