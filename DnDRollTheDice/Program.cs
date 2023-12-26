f3c60c7cff82b83e2fcc7489cb811e93d934329e
ArthurMabuti <arthurmabuti@hotmail.com>
2023-12-26 11:19:59 -0300

    Testing ModifierValue() function

--- DnDRollTheDice/Program.cs
+++ DnDRollTheDice/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DnDRollTheDice.Character;
+
+//Testing Modifier Value
+CharacterStatus bruenor = new()
+{
+    Name = "Bruenor",
+    Class = "Fighter",
+    Dexterity = 20
+};
+Console.WriteLine(bruenor.ModifierValue(bruenor.Dexterity));
