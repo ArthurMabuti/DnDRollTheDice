1cf787ed1ca83729578e9551eb86a3e09c7bb54b
ArthurMabuti <arthurmabuti@hotmail.com>
2023-12-26 11:18:41 -0300

    Created a class to add attributes for players' characters

---
+++ DnDRollTheDice/Character/CharacterStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace DnDRollTheDice.Character;
+
+internal class CharacterStatus
+{
+    public string? Name { get; set; }
+    public string? Class { get; set; }
+    public int HitPoints { get; set; }
+    public int ArmorClass { get; set; }
+    public int Initiative { get; set; }
+    public int Speed { get; set; }
+    public int Strength { get; set; }
+    public int Dexterity { get; set; }
+    public int Constitution { get; set; }
+    public int Intelligence { get; set; }
+    public int Wisdom { get; set; }
+    public int Charisma { get; set; }
+}
