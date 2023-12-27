4775a4b7dc67a6ab8209f730d035a56317944cf3
ArthurMabuti <arthurmabuti@hotmail.com>
2023-12-27 15:33:22 -0300

    Created class for Monsters for creating a combat against player characters

---
+++ DnDRollTheDice/Character/Monster.cs
@@ -0,0 +1,39 @@
+﻿using DnDRollTheDice.Character.CharacterDetails;
+using DnDRollTheDice.Items;
+using System.Text.Json.Serialization;
+
+namespace DnDRollTheDice.Character;
+internal class Monster : CharacterStatus
+{
+    [JsonPropertyName("name")]
+    public new string? Name { get; set; }
+    [JsonPropertyName("armor_class")]
+    public new List<ArmorClass> ArmorClass { get; set; }
+    [JsonPropertyName("hit_points")]
+    public new int HitPoints { get; set; }
+    [JsonPropertyName("speed")]
+    public new Speed Speed { get; set; }
+    [JsonPropertyName("strength")]
+    public int Strength { get; set; }
+    [JsonPropertyName("dexterity")]
+    public int Dexterity { get; set; }
+    [JsonPropertyName("constitution")]
+    public int Constitution { get; set; }
+    [JsonPropertyName("intelligence")]
+    public int Intelligence { get; set; }
+    [JsonPropertyName("wisdom")]
+    public int Wisdom { get; set; }
+    [JsonPropertyName("charisma")]
+    public int Charisma { get; set; }
+
+    public Monster()
+    {
+        ArmorClass = new List<ArmorClass>();
+        Speed = new Speed();
+    }
+
+    public override string? ToString()
+    {
+        return $"Monster: {Name} | AC {ArmorClass.First().Value} | HP {HitPoints}";
+    }
+}
