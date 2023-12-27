981f78e323578927a3ccc3453e70d0610b470c81
ArthurMabuti <arthurmabuti@hotmail.com>
2023-12-27 10:49:54 -0300

    Created GenerateRandomAbilityScore() function

--- DnDRollTheDice/Character/CharacterStatus.cs
+++ DnDRollTheDice/Character/CharacterStatus.cs
@@ -16,18 +16,40 @@ internal class CharacterStatus
     public int Wisdom { get; set; }
     public int Charisma { get; set; }
 
+    public CharacterStatus()
+    {
+
+    }
+
     public int ModifierValue(int abilityScore)
     {
         double modifier = (double)(abilityScore - 10) / 2;
         return (int)Math.Floor(modifier);
     }
 
-    public void InitiativeCheck(int dexterity)
+    public void InitiativeCheck()
     {
         int rollValue = Roll.DiceRoll(1, 20);
-        int finalResult = rollValue + ModifierValue(dexterity);
+        int finalResult = rollValue + ModifierValue(this.Dexterity);
+        Console.WriteLine($"The dice roll for initiative was {rollValue}");
         Initiative = finalResult;
     }
 
-    public void 
+    public int GenerateRandomAbilityScore()
+    {
+        //Roll 4 6-sized dice
+        List<int> rollsResult = new();
+        for(int i = 0; i < 4; i++)
+        {
+            int roll = Roll.DiceRoll(1, 6);
+            rollsResult.Add(roll);
+        }
+        
+        rollsResult = rollsResult.OrderBy(x => x).ToList();
+
+        //Remove the lowest one
+        rollsResult.RemoveAt(0);
+
+        return rollsResult.Sum(); 
+    }
 }
