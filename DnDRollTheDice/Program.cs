b2b62b72b1d0e3435b1182da3feba93c8a9f68a4
ArthurMabuti <arthurmabuti@hotmail.com>
2023-12-27 11:07:01 -0300

    Created an Interface() to easily see the Character Ability Scores

--- DnDRollTheDice/Program.cs
+++ DnDRollTheDice/Program.cs
@@ -6,14 +6,31 @@ CharacterStatus bruenor = new()
 {
     Name = "Bruenor",
     Class = "Fighter",
-    Dexterity = 10
 };
-Console.WriteLine(bruenor.ModifierValue(bruenor.Dexterity));
+bruenor.CharacterWithRandomAbilityScore();
+
+Interface(bruenor);
 
 //Testing Dice Roll
 //Fireball
 Console.WriteLine($"Damage from Fireball: {Roll.DiceRoll(8, 6)}");
 
 //Testing Initiative
-bruenor.InitiativeCheck(bruenor.Dexterity);
+Console.WriteLine($"Bruenor Dexterity: {bruenor.Dexterity}");
+bruenor.InitiativeCheck();
 Console.WriteLine($"Bruenor initiative value is {bruenor.Initiative}");
+
+//Testing RandomAbilityScore
+bruenor.GenerateRandomAbilityScore();
+
+void Interface(CharacterStatus character)
+{
+    Console.WriteLine($@"       {character.Name}
+For: {character.Strength} | {bruenor.ModifierValue(bruenor.Strength)}
+Dex: {character.Dexterity} | {bruenor.ModifierValue(bruenor.Dexterity)}
+Con: {character.Constitution} | {bruenor.ModifierValue(bruenor.Constitution)}
+Int: {character.Intelligence} | {bruenor.ModifierValue(bruenor.Intelligence)}
+Wis: {character.Wisdom} | {bruenor.ModifierValue(bruenor.Wisdom)}
+Cha: {character.Charisma} | {bruenor.ModifierValue(bruenor.Charisma)}
+");
+}
