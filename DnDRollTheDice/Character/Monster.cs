29447d9c52ef27ee6b6bd4b5414bf742f5e0c5e9
ArthurMabuti <arthurmabuti@hotmail.com>
2023-12-29 11:46:42 -0300

    Created SettingAbilityScores() method and UseManualStatus() for offline purposes

--- DnDRollTheDice/Character/Monster.cs
+++ DnDRollTheDice/Character/Monster.cs
@@ -3,7 +3,7 @@ using DnDRollTheDice.Items;
 using System.Text.Json.Serialization;
 
 namespace DnDRollTheDice.Character;
-internal class Monster : CharacterStatus
+public class Monster : CharacterStatus
 {
     [JsonPropertyName("name")]
     public new string? Name { get; set; }
@@ -28,8 +28,40 @@ internal class Monster : CharacterStatus
 
     public Monster()
     {
+        AbilityScores = new Dictionary<string, int>();
         ArmorClass = new List<ArmorClass>();
         Speed = new Speed();
+    }
+
+    public void SettingAbilityScores()
+    {
+        foreach (var property in typeof(Monster).GetProperties())
+        {
+            if (property.Name == "HitPoints")
+            {
+                continue;
+            }
+
+            if (property.PropertyType == typeof(int) && property.GetCustomAttributes(typeof(JsonPropertyNameAttribute), true).Length > 0)
+            {
+                var attributeName = ((JsonPropertyNameAttribute)property.GetCustomAttributes(typeof(JsonPropertyNameAttribute), true)[0]).Name;
+                AbilityScores.Add(property.Name, (int)property.GetValue(this));
+            }
+        }
+    }
+
+    public void UseManualStatus()
+    {
+        Name = "Goblin";
+        ArmorClass.Add(new() { Type = "armor", Value = 15 });
+        HitPoints = 7;
+        Speed = new() { Locomotion = "9m"};
+        Strength = 8;
+        Dexterity = 14;
+        Constitution = 10;
+        Intelligence = 10;
+        Wisdom = 8;
+        Charisma = 8;
     }
 
     public override string? ToString()
