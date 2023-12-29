3befadfbc6c391c3175373a57727daeb1ae99a98
ArthurMabuti <arthurmabuti@hotmail.com>
2023-12-29 11:42:07 -0300

    Created a service class to encapsulate a method to call a monster by name

---
+++ DnDRollTheDice/Services/MonsterService.cs
@@ -0,0 +1,18 @@
+﻿using DnDRollTheDice.Character;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DnDRollTheDice.Services;
+public class MonsterService
+{
+    public async Task<Monster?> GetMonsterFromApiAsync(string monster)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string answer = await client.GetStringAsync($"https://www.dnd5eapi.co/api/monsters/{monster}/");
+            Monster? newMonster = JsonSerializer.Deserialize<Monster>(answer);
+            newMonster?.SettingAbilityScores();
+            return newMonster;
+        }
+    }
+}
