eb3d9bfc6c9300fe3226e532065531c963c62ba4
pereira.arthur.ext <pereira.arthur.ext@br.panasonic.com>
2024-01-02 12:18:48 -0300

    Renamed the class and moved properties and methods shared with the Monster class to the Character class

---
+++ DnDRollTheDice/Character/PlayerCharacter.cs
@@ -0,0 +1,49 @@
+﻿using DnDRollTheDice.Character.CharacterDetails;
+using DnDRollTheDice.Character.CharacterItems;
+
+namespace DnDRollTheDice.Character;
+
+internal class PlayerCharacter : Character
+{
+    public string Class { get; set; }
+    public PlayerCharacter()
+    {
+        CreatingAbilityScores();
+    }
+
+    private void CreatingAbilityScores()
+    {
+        string[] abilityNames = { "Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma" };
+
+        foreach (string abilityName in abilityNames)
+        {
+            AbilityScores.Add(abilityName, 0);
+        }
+    }
+
+    public int GenerateRandomAbilityScore()
+    {
+        //Roll 4 6-sized dice
+        List<int> rollsResult = new();
+        for (int i = 0; i < 4; i++)
+        {
+            int roll = Roll.DiceRoll(1, 6);
+            rollsResult.Add(roll);
+        }
+
+        rollsResult = rollsResult.OrderBy(x => x).ToList();
+
+        //Remove the lowest one
+        rollsResult.RemoveAt(0);
+
+        return rollsResult.Sum();
+    }
+
+    public void CharacterWithRandomAbilityScore()
+    {
+        foreach (string abilityName in AbilityScores.Keys.ToList())
+        {
+            AbilityScores[abilityName] = GenerateRandomAbilityScore();
+        }
+    }
+}
