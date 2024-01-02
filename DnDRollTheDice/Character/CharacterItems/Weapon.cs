14cd78714dfa24f487098264728564061f4bb8fe
pereira.arthur.ext <pereira.arthur.ext@br.panasonic.com>
2024-01-02 16:11:28 -0300

    Created class so it can be possible to store API weapon information

---
+++ DnDRollTheDice/Character/CharacterItems/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using DnDRollTheDice.Character.CharacterDetails;
+using System.Text.Json.Serialization;
+
+namespace DnDRollTheDice.Character.CharacterItems;
+
+internal class Weapon
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+    [JsonPropertyName("weapon_range")]
+    public string? Range { get; set; }
+    [JsonPropertyName("damage")]
+    public Damage? Damage { get; set; }
+
+    public Weapon()
+    {
+        Damage = new Damage();
+    }
+
+    public int DamageRoll()
+    {
+        string[]? diceRollInformation = Damage?.DamageDice?.Split('d');
+        int numberOfDices = int.Parse(diceRollInformation[0]);
+        int diceSides = int.Parse(diceRollInformation[1]);
+
+        int rollValue = Roll.DiceRoll(numberOfDices, diceSides);
+        return rollValue;
+    }
+}
