a408bf8e15d4d1b4e532bfe5896dc694de029679
pereira.arthur.ext <pereira.arthur.ext@br.panasonic.com>
2024-01-02 16:11:10 -0300

    Created class so it can store API damage information

---
+++ DnDRollTheDice/Character/CharacterDetails/Damage.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DnDRollTheDice.Character.CharacterDetails;
+public class Damage
+{
+    [JsonPropertyName("damage_dice")]
+    public string? DamageDice { get; set; }
+}
