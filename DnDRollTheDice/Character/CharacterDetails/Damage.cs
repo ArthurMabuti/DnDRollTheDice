9482187a282b2e5a2b6cc58f072e1df352221555
pereira.arthur.ext <pereira.arthur.ext@br.panasonic.com>
2024-01-03 16:06:09 -0300

    Send DamageRoll() method to Damage class because monsters don't necessarily have a weapon

--- DnDRollTheDice/Character/CharacterDetails/Damage.cs
+++ DnDRollTheDice/Character/CharacterDetails/Damage.cs
@@ -5,4 +5,20 @@ public class Damage
 {
     [JsonPropertyName("damage_dice")]
     public string? DamageDice { get; set; }
+
+    public int DamageRoll(bool criticalHit)
+    {
+        string[]? diceRollInformation = DamageDice?.Split('d');
+        int numberOfDices = int.Parse(diceRollInformation![0]);
+        if (criticalHit)
+        {
+            numberOfDices *= 2;
+            Console.WriteLine("*CRITICAL HIT*");
+        }
+
+        int diceSides = int.Parse(diceRollInformation[1]);
+
+        int rollValue = Roll.DiceRoll(numberOfDices, diceSides);
+        return rollValue;
+    }
 }
