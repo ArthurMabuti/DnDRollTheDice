300327f2829bb4599d2e537101d40f701836df80
pereira.arthur.ext <pereira.arthur.ext@br.panasonic.com>
2024-01-03 15:33:59 -0300

    Created class to store monster actions details

---
+++ DnDRollTheDice/Character/CharacterDetails/Actions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DnDRollTheDice.Character.CharacterDetails;
+public class Actions
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("desc")]
+    public string Description { get; set; }
+    [JsonPropertyName("damage")]
+    public List<Damage> Damage { get; set; }
+}
