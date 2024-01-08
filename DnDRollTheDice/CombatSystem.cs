872c035be43550414262d55e2ab0f2082b6957ea
ArthurMabuti <arthurmabuti@hotmail.com>
2024-01-08 12:35:07 -0300

    Created EnganeInBattle() method to simulate a combat between PlayerCharacter and Monsters

--- DnDRollTheDice/CombatSystem.cs
+++ DnDRollTheDice/CombatSystem.cs
@@ -10,6 +10,32 @@ internal class CombatSystem
         AllCharacters = []; 
     }
 
+    public static void EngageInBattle()
+    {
+        List<Monster> allMonsters = GetAllCharactersOfType<Monster>();
+        List<PlayerCharacter> allPlayerCharacters = GetAllCharactersOfType<PlayerCharacter>();
+
+        OrderCharactersByInitiative();
+
+        while (!allPlayerCharacters.All(cha => cha.IsUnconscious()) && !allMonsters.All(mon => mon.IsUnconscious()))
+        {
+            foreach (var character in AllCharacters)
+            {
+                
+                if (character is PlayerCharacter playerCharacter)
+                {
+                    if (character.HitPoints <= 0) break;
+                    playerCharacter.DealingDamage(allMonsters);
+                    continue;
+                }
+                if (character is Monster monsterCharacter && monsterCharacter.HitPoints > 0)
+                {
+                    monsterCharacter.AttackAction(allPlayerCharacters);
+                }
+            }
+        }
+    }
+
     private static void OrderCharactersByInitiative()
     {
         AllCharacters = AllCharacters.OrderByDescending(c => c.Initiative).ToList();
