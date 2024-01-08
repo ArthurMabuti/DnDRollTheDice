9443282e32d0e3a9ba0029fa7b26d6385c6c2812
ArthurMabuti <arthurmabuti@hotmail.com>
2024-01-08 12:33:35 -0300

    Moved OrderByInitiative() method from Program.cs and updated to CombatSystem class purposes

--- DnDRollTheDice/CombatSystem.cs
+++ DnDRollTheDice/CombatSystem.cs
@@ -10,6 +10,10 @@ internal class CombatSystem
         AllCharacters = []; 
     }
 
+    private static void OrderCharactersByInitiative()
+    {
+        AllCharacters = AllCharacters.OrderByDescending(c => c.Initiative).ToList();
+    }
     public static void AddCharacter(CharacterType character)
     {
         AllCharacters.Add(character);
