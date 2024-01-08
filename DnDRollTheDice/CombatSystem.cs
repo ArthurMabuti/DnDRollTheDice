b2884d41fbda8fa6dc03e95c562474e3ff970287
ArthurMabuti <arthurmabuti@hotmail.com>
2024-01-08 11:28:06 -0300

    Created a new class to store All Characters Type created

---
+++ DnDRollTheDice/CombatSystem.cs
@@ -0,0 +1,17 @@
+﻿using CharacterType = DnDRollTheDice.Character.Character;
+
+namespace DnDRollTheDice;
+internal class CombatSystem
+{
+    public static List<CharacterType> AllCharacters { get; set; }
+
+    static CombatSystem() 
+    {  
+        AllCharacters = []; 
+    }
+
+    public static void AddCharacter(CharacterType character)
+    {
+        AllCharacters.Add(character);
+    }
+}
