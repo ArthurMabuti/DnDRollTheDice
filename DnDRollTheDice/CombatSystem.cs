4c3371504d7dad29234a6741d2ed02d17da25b89
ArthurMabuti <arthurmabuti@hotmail.com>
2024-01-08 12:34:23 -0300

    Created GetAllCharacterOfType<T>() method to separate lists of Monsters and PlayerCharacters

--- DnDRollTheDice/CombatSystem.cs
+++ DnDRollTheDice/CombatSystem.cs
@@ -14,6 +14,20 @@ internal class CombatSystem
     {
         AllCharacters = AllCharacters.OrderByDescending(c => c.Initiative).ToList();
     }
+
+    private static List<T> GetAllCharactersOfType<T>() where T : CharacterType
+    {
+        List<T> charactersOfType = [];
+        foreach (var character in AllCharacters)
+        {
+            if (character is T typedCharacter)
+            {
+                charactersOfType.Add(typedCharacter);
+            }
+        }
+        return charactersOfType;
+    }
+
     public static void AddCharacter(CharacterType character)
     {
         AllCharacters.Add(character);
