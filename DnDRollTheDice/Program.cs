4f5077cd640f89a638b84a038c70548cc754e8c7
ArthurMabuti <arthurmabuti@hotmail.com>
2024-01-08 12:44:00 -0300

    Tested CombatSystem EngageInBattle() method

--- DnDRollTheDice/Program.cs
+++ DnDRollTheDice/Program.cs
@@ -1,12 +1,11 @@
-﻿using DnDRollTheDice.Character;
+﻿using DnDRollTheDice;
+using DnDRollTheDice.Character;
 using DnDRollTheDice.Character.CharacterItems;
 using DnDRollTheDice.Services;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Xml.Linq;
 
 #region Testing Character Status
 
-//Testing Modifier Value
 PlayerCharacter bruenor = new()
 {
     Name = "Bruenor",
@@ -16,8 +15,6 @@ PlayerCharacter bruenor = new()
 };
 bruenor.ArmorClass = new ArmorClass() { Type = "Armor", Value = 18, Armor = new List<Armor>() };
 bruenor.CharacterWithRandomAbilityScore();
-
-bruenor.Interface();
 
 //Testing Dice Roll
 //Fireball
@@ -76,50 +73,6 @@ bruenor.Weapon = greatsword!;
 
 #region Testing Initiative Order
 
-List<Character> allCharacters = new()
-{
-    bruenor,
-    monster1,
-    monster2
-};
-
-List<string> charactersNameByInitiative = OrderByInitiative(allCharacters);
-
-while(bruenor.HitPoints > 0 && (monster1.HitPoints > 0 || monster2.HitPoints > 0))
-{
-    foreach (var characterName in charactersNameByInitiative)
-    {
-        Character activeCharacter = allCharacters.Find(cha => cha.Name == characterName)!;
-        if (bruenor.HitPoints <= 0) break;
-        if (activeCharacter is PlayerCharacter playerCharacter)
-        {
-            if (monster1.HitPoints > 0)
-                playerCharacter.DealingDamage(monster1);
-            else playerCharacter.DealingDamage(monster2);
-            continue;
-        }
-        if (activeCharacter is Monster monsterCharacter && monsterCharacter.HitPoints > 0)
-        {
-            Console.WriteLine("Which action do you want to use to attack?");
-            foreach (var monsterAction in monsterCharacter!.Actions)
-            {
-                Console.WriteLine(monsterAction.Name);
-            }
-            string attackOption = Console.ReadLine()!;
-            monsterCharacter.AttackAction(bruenor, attackOption);
-        }
-    }
-}
+CombatSystem.EngageInBattle();
 
 #endregion
-
-List<string> OrderByInitiative(List<Character> characters)
-{
-    List<Character> list = characters.OrderByDescending(c => c.Initiative).ToList();
-    List<string> orderList = new List<string>();
-    foreach(var character in list)
-    {
-        orderList.Add(character.Name!);
-    }
-    return orderList;
-}
