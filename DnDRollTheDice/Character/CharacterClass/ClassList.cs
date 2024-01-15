1dcff732c85788737935c991014622e49d7a084b
ArthurMabuti <arthurmabuti@hotmail.com>
2024-01-15 11:29:53 -0300

    Created ClassList

---
+++ DnDRollTheDice/Character/CharacterClass/ClassList.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DnDRollTheDice.Character.CharacterClass;
+internal class ClassList
+{
+    [JsonPropertyName("results")]
+    public List<Class>? Class { get; set; }
+}
