75494cd2a058b030ad8e315287d72cfc4943a2fd
ArthurMabuti <arthurmabuti@hotmail.com>
2024-01-15 11:29:43 -0300

    Created Class class and FormatAndSplit method

---
+++ DnDRollTheDice/Character/CharacterClass/Class.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
+namespace DnDRollTheDice.Character.CharacterClass;
+internal class Class
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+    [JsonPropertyName("desc")]
+    public string? Description { get; set; }
+    [JsonPropertyName("hit_dice")]
+    public string? HitPointsDice { get; set; }
+    [JsonPropertyName("prof_weapons")]
+    private string? proficiencyWeapons { get; set; }
+    public List<string>? ProficiencyWeapons => FormatAndSplit(proficiencyWeapons!);
+    [JsonPropertyName("prof_saving_throws")]
+    private string? proficiencySavingThrow { get; set; }
+    public List<string>? ProficiencySavingThrow => FormatAndSplit(proficiencySavingThrow!);
+    [JsonPropertyName("spellcasting_ability")]
+    public string? SpellCastingAbility { get; set; }
+
+    private List<string> FormatAndSplit(string input)
+    {
+        // Remove 's' at the end of each word
+        string result = Regex.Replace(input, @"\b(\w+?)s\b", "$1", RegexOptions.IgnoreCase);
+
+        // Split the words using ", "
+        List<string> formattedList = result.Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        return formattedList;
+    }
+}
