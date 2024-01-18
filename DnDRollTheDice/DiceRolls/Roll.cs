ce6cf4fa53b733d1211ae80ee35be87895019ce9
ArthurMabuti <arthurmabuti@hotmail.com>
2024-01-17 21:40:18 -0300

    Created DisadvantageDiceRoll() and AdvantageDiceRoll() methods

---
+++ DnDRollTheDice/DiceRolls/Roll.cs
@@ -0,0 +1,33 @@
+﻿namespace DnDRollTheDice.DiceRolls;
+
+internal class Roll
+{
+    public int Sides { get; set; }
+    public int NumberOfDices { get; set; }
+
+    public static int DiceRoll(int numberOfDices, int sides)
+    {
+        Random random = new();
+        int result = 0;
+        int repetition = 0;
+        while (repetition < numberOfDices)
+        {
+            int diceRoll = random.Next(1, sides + 1);
+            result += diceRoll;
+            repetition++;
+        }
+        return result;
+    }
+
+    public static int DisadvantageDiceRoll()
+    {
+        // Return the lower DiceRoll
+        return Math.Min(DiceRoll(1, 20), DiceRoll(1, 20));
+    }
+
+    public static int AdvantageDiceRoll()
+    {
+        // Return the higher DiceRoll
+        return Math.Max(DiceRoll(1, 20), DiceRoll(1, 20));
+    }
+}
