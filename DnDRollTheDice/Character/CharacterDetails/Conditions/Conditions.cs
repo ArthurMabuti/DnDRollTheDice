a074d71bd500297289203a338a9046d94f19e49e
ArthurMabuti <arthurmabuti@hotmail.com>
2024-01-30 15:45:53 -0300

    Created Conditions class

---
+++ DnDRollTheDice/Character/CharacterDetails/Conditions/Conditions.cs
@@ -0,0 +1,17 @@
+﻿namespace DnDRollTheDice.Character.CharacterDetails.Conditions;
+internal enum Conditions
+{
+    Blinded,
+    Charmed,
+    Frightened,
+    Grappled,
+    Incapacitated,
+    Invisible,
+    Paralyzed,
+    Petrified,
+    Poisoned,
+    Prone,
+    Restrained,
+    Stunned,
+    Unconscious
+}
