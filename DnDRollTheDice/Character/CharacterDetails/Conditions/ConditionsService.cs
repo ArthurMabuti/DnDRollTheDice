4b56435d178b86dfa37ddb45add4ec1c3e3dce66
ArthurMabuti <arthurmabuti@hotmail.com>
2024-02-02 11:25:43 -0300

    Created ConditionsService class

---
+++ DnDRollTheDice/Character/CharacterDetails/Conditions/ConditionsService.cs
@@ -0,0 +1,15 @@
+﻿namespace DnDRollTheDice.Character.CharacterDetails.Conditions;
+internal class ConditionsService
+{
+    public static void ApplyPoisonedCondition(Character character)
+    {
+        // Implementação para aplicar a condição "Poisoned" ao personagem
+    }
+
+    public static void RemoveFrightenedCondition(Character character)
+    {
+        // Implementação para remover a condição "Frightened" do personagem
+    }
+
+    // Outros métodos relacionados às condições
+}
