595d6ee50eb24a3c5060322c63e34c4fc6d307ec
ArthurMabuti <arthurmabuti@hotmail.com>
2024-02-02 11:25:30 -0300

    Created ConditionsExtensions class

---
+++ DnDRollTheDice/Character/CharacterDetails/Conditions/ConditionsExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace DnDRollTheDice.Character.CharacterDetails.Conditions;
+internal static class ConditionsExtensions
+{
+    public static bool IsPoisoned(this Conditions conditions)
+    {
+        return conditions.HasFlag(Conditions.Poisoned);
+    }
+
+    public static bool IsFrightened(this Conditions conditions)
+    {
+        return conditions.HasFlag(Conditions.Frightened);
+    }
+
+    // Outros métodos de extensão relacionados às condições
+}
