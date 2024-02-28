2dba0677e612bdbb6e189514799ccbe5edcd5e27
pereira.arthur.ext <pereira.arthur.ext@br.panasonic.com>
2024-02-28 11:56:53 -0300

    Created ApplyCondition method

--- DnDRollTheDice/Character/CharacterDetails/Conditions/ConditionsService.cs
+++ DnDRollTheDice/Character/CharacterDetails/Conditions/ConditionsService.cs
@@ -1,6 +1,21 @@
 ﻿namespace DnDRollTheDice.Character.CharacterDetails.Conditions;
 internal class ConditionsService
 {
+    public void ApplyCondition(Conditions condition, Character character)
+    {
+        string methodName = $"Apply{condition}Condition";
+        MethodInfo method = GetType().GetMethod(methodName)!;
+
+        if (method != null)
+        {
+            method.Invoke(this, new object[] { character });
+            character.Conditions!.Add(condition);
+        }
+        else
+        {
+            Console.WriteLine($"Método para aplicar a condição {condition} não encontrado.");
+        }
+    }
     public static void ApplyPoisonedCondition(Character character)
     {
         // Implementação para aplicar a condição "Poisoned" ao personagem
