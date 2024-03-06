48c25bb503e6dca5e95063e5b93ae97d09b731f2
pereira.arthur.ext <pereira.arthur.ext@br.panasonic.com>
2024-03-06 15:24:53 -0300

    Created UtilityClass with methods that can be used in all project

---
+++ DnDRollTheDice/UtilityClass.cs
@@ -0,0 +1,29 @@
+﻿namespace DnDRollTheDice;
+internal class UtilityClass
+{
+    public static string FirstLetterUpper(string text)
+    {
+        return char.ToUpper(text[0]) + text[1..];
+    }
+
+    public static string FormatNameWithSpaces(string targetName)
+    {
+        // Verifies if the targetName has a composed name, if yes, stores each name in the full name variable, then stores it in targetName
+        if (targetName.Split(" ").Length >= 2)
+        {
+            string[] composedName = targetName.Split(" ");
+            string fullName = "";
+            foreach (string name in composedName)
+            {
+                // Store each name with the first char upper
+                fullName += FirstLetterUpper(name) + " ";
+            }
+            return fullName.TrimEnd();
+        }
+        // If has a single name, just Upper the first letter
+        else
+        {
+            return FirstLetterUpper(targetName);
+        }
+    }
+}
